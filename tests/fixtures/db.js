48e5eda0e020c4decfde095bb0e699383e40b2bf
ndtrang121 <ndtrang129@gmail.com>
2022-06-08 13:57:04 +0700

    update db.js

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -5,64 +5,64 @@ const Task = require('../../src/models/task')
 
 const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
-    _id: userOneId,
-    name: 'Mike',
-    email: 'mike@example.com',
-    password: '56what!!',
-    tokens: [{
-        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
-    }]
+	_id: userOneId,
+	name: 'Mike',
+	email: 'mike@example.com',
+	password: '56what!!',
+	tokens: [ {
+		token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+	} ]
 }
 
 const userTwoId = new mongoose.Types.ObjectId()
 const userTwo = {
-    _id: userTwoId,
-    name: 'Jess',
-    email: 'jess@example.com',
-    password: 'myhouse099@@',
-    tokens: [{
-        token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET)
-    }]
+	_id: userTwoId,
+	name: 'Jess',
+	email: 'jess@example.com',
+	password: 'myhouse099@@',
+	tokens: [ {
+		token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET)
+	} ]
 }
 
 const taskOne = {
-    _id: new mongoose.Types.ObjectId(),
-    description: 'First task',
-    completed: false,
-    owner: userOne._id
+	_id: new mongoose.Types.ObjectId(),
+	description: 'First task',
+	completed: false,
+	owner: userOne._id
 }
 
 const taskTwo = {
-    _id: new mongoose.Types.ObjectId(),
-    description: 'Second task',
-    completed: true,
-    owner: userOne._id
+	_id: new mongoose.Types.ObjectId(),
+	description: 'Second task',
+	completed: true,
+	owner: userOne._id
 }
 
 const taskThree = {
-    _id: new mongoose.Types.ObjectId(),
-    description: 'Third task',
-    completed: true,
-    owner: userTwo._id
+	_id: new mongoose.Types.ObjectId(),
+	description: 'Third task',
+	completed: true,
+	owner: userTwo._id
 }
 
 const setupDatabase = async () => {
-    await User.deleteMany()
-    await Task.deleteMany()
-    await new User(userOne).save()
-    await new User(userTwo).save()
-    await new Task(taskOne).save()
-    await new Task(taskTwo).save()
-    await new Task(taskThree).save()
+	await User.deleteMany()
+	await Task.deleteMany()
+	await new User(userOne).save()
+	await new User(userTwo).save()
+	await new Task(taskOne).save()
+	await new Task(taskTwo).save()
+	await new Task(taskThree).save()
 }
 
 module.exports = {
-    userOneId,
-    userOne,
-    userTwoId,
-    userTwo,
-    taskOne,
-    taskTwo,
-    taskThree,
-    setupDatabase
+	userOneId,
+	userOne,
+	userTwoId,
+	userTwo,
+	taskOne,
+	taskTwo,
+	taskThree,
+	setupDatabase
 }
